107aaf66537d5e035f33b6daa19b45de8cf273cb
Benjamin <ben.chesser19@gmail.com>
2023-05-11 20:46:51 -0700

    Added controller

---
+++ controllers/languages_controller.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const languages = express.Router();
+const Language = require('../models/language');
+
+module.exports = languages;
--- server.js
+++ server.js
@@ -13,6 +13,10 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
 
+//Languages controller
+const languageController = require('./controllers/languages_controller');
+app.use('/languages', languageController);
+
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to the Hello World! API')
